dd6bb34e273aa4ec87d0ea96cff3d1e782f5e813
Soyn <w604408482@163.com>
2016-04-06 21:28:40 +0800

    finshed exercise1

---
+++ shilina/c_exercises/c_variables_and_datatypes_exercises/exercise1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/******************************************************
+* @Author: shilina
+* @Brief: Exercise 1: Write a C program to print the
+*  following line as shown below:
+*  Welcome!
+*  You are able to test your skill of writing C code here.
+*  @CreatedTime: 6/4/16.
+*********************************************************/
+
+int main(int argc, char **argv)
+{
+    printf("Welcome!\n");
+    printf("You are able to test your skill of writing C code here.");
+     return 0;
+}
