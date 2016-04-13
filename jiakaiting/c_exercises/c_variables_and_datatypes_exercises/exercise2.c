07b9b58f09e56ccf6edbe1d49f3492ef29ad84cb
JIaJiating <1160458697@qq.com>
2016-04-13 20:51:48 +0800

    add exercise2

---
+++ jiakaiting/c_exercises/c_variables_and_datatypes_exercises/exercise2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/************************************************
+* @Author: jiakaiting
+* @Brief:Write five statements by using printf function to print the asterisk pattern.
+* @CreatedTime: 13/4/16.
+*************************************************/
+int main(int argc, char *argv[])
+{
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
