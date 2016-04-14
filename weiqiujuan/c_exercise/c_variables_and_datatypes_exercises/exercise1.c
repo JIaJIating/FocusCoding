70f06fa7d1a080b7dfe11b2018d696df4ea68033
CallmePapa <zhongli_wei@xa.foxmail.com>
2016-04-14 19:00:11 +0800

    my exercise

--- weiqiujuan/c_exercise/c_variables_and_datatypes_exercises/exercise1.c
+++ weiqiujuan/c_exercise/c_variables_and_datatypes_exercises/exercise1.c
@@ -8,7 +8,6 @@
 int main(int argc, char *argv[]) 
 { 
   printf(" Welcome!\n"); 
-  printf(" You are able to test your skill of writing C code here.\n"); 
-  system("PAUSE");      
+  printf(" You are able to test your skill of writing C code here.\n");       
   return 0; 
 }  
