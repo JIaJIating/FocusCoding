64dfb7669abd21411f3a3e3d81afc67b25d705a0
unknown <>
2016-05-06 12:24:11 +0800

    finished exercise4

--- shilina/c_exercises/c_variables_and_datatypes_exercises/exercise4.c
+++ shilina/c_exercises/c_variables_and_datatypes_exercises/exercise4.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include<stdlib.h>
-/************************************************************************************************************
+/*******************************************************************
 * @Author:shilina
-* @Brief:Write a C program to prompt the user to input her/his age and print it on the screen, as shown below.
+* @Brief: Write a C program to prompt the user to input her/his age
+*         and print it on the screen, as shown below.
 * @CreatedTime:4/5/16
-**************************************************************************************************************/
+*******************************************************************/
 
 int main(int argc,char *argv[])
 {
